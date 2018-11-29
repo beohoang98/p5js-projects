3045f54e6669c04bc4a283a0f5a93cb936429b15
beohoang98 <beohoang98@users.noreply.github.com>
2018-11-29 11:34:34 +0700

    create main site

---
+++ script.js
@@ -0,0 +1,39 @@
+document.addEventListener('DOMContentLoaded', () => {
+    main();
+});
+
+async function main() {
+    const listElement = document.querySelector('#project-list');
+
+    const projects = await getProjectsData();
+    document.querySelector('#loading').style.display = 'none';
+
+    for (const project of projects) {
+        listElement.append(createProjectCard(project));
+    }
+}
+
+async function getProjectsData() {
+    const res = await fetch('projects.json');
+    const json = await res.json();
+    return json.projects;
+}
+
+function createProjectCard(project) {
+    const card = document.createElement('article');
+    card.className = 'project';
+    card.innerHTML = `
+    <div class='project-thumb-image'>
+        <img alt='${project.name}' src='${project.thumbnail_image}'/>
+    </div>
+    <div class='project-info'>
+        <div class='project-name'>${project.name}</div>
+        <div class='project-des'>${project.description}</div>
+    </div>
+    <div class='project-link'>
+        <a href='${project.path}' target='_blank' rel='noopenner noreferrer'>See project</a>
+    </div>
+    `;
+
+    return card;
+}
